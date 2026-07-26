d9cdccc0a8fbf12a8b58c86fd21f24eeaa89caa0
KevinSex08 <erazok168@gmail.com>
2026-07-26 02:03:30 -0500

    chore: inicializar estructura base del proyecto Spring Boot y carpetas de arquitectura

---
+++ src/main/kotlin/com/example/canchaYa/CanchaYaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.canchaYa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CanchaYaApplication
+
+fun main(args: Array<String>) {
+	runApplication<CanchaYaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/canchaYa/CanchaYaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.canchaYa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CanchaYaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
