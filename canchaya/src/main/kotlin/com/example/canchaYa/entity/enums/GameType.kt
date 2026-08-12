085ed726e9fb7133571519d554ffb73f81bdcf1c
Ariel Rueda <agrueda@puce.edu.ec>
2026-08-12 01:17:09 -0500

    fix(canchaya): use JsonProperty to map CLASSIC to TRADITIONAL to prevent DB corruption

--- canchaya/src/main/kotlin/com/example/canchaYa/entity/enums/GameType.kt
+++ canchaya/src/main/kotlin/com/example/canchaYa/entity/enums/GameType.kt
@@ -1,6 +1,9 @@
 package com.example.canchaYa.entity.enums
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 enum class GameType {
     SUPER_8,
-    CLASSIC
+    @JsonProperty("CLASSIC")
+    TRADITIONAL
 }
